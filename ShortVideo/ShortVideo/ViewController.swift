2a1e11970d33f0edb5d66ab2ba236f5d01fd2285
tranquoccuong <cuongtq4998@gmail.com>
2022-11-02 21:11:32 +0700

    init source

---
+++ ShortVideo/ShortVideo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShortVideo
+//
+//  Created by Quoc Cuong on 02/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
